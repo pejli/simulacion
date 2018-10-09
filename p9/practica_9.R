639e4a7eeacd1ce8269750436c6002781d2d8bcd
pejli <42283649+pejli@users.noreply.github.com>
2018-10-08 23:51:56 -0500

    Add files via upload

---
+++ p9/practica_9.R
@@ -0,0 +1,44 @@
+n<-200
+p <- data.frame(x = rnorm(n), y=rnorm(n), c=rnorm(n),m=rnorm(n))
+xmax <- max(p$x)
+xmin <- min(p$x)
+p$x <- (p$x - xmin) / (xmax - xmin) # ahora son de 0 a 1
+ymax <- max(p$y)
+ymin <- min(p$y)
+p$y <- (p$y - ymin) / (ymax - ymin) # las y tambien
+cmax <- max(p$c)
+cmin <- min(p$c)
+p$c <- 2 * (p$c - cmin) / (cmax - cmin) - 1 # cargas son entre -1 y 1cmax <- max(p$c)
+mmin <- min(p$m)
+mmax<-max(p$m)
+p$m <- 4.9 * (p$m - mmin) / (mmax - mmin) +0.1
+eps <- 0.001
+tmax<-100
+fuerza <- function(i) {
+  xi <- p[i,]$x
+  yi <- p[i,]$y
+  ci <- p[i,]$c
+  fx <- 0
+  fy <- 0
+  for (j in 1:n) {
+    cj <- p[j,]$c
+    dir <- (-1)^(1 + 1 * (ci * cj < 0))
+    dx <- xi - p[j,]$x
+    dy <- yi - p[j,]$y
+    factor <- dir * abs(ci - cj) / (sqrt(dx^2 + dy^2) + eps)
+    fx <- fx - dx * factor
+    fy <- fy - dy * factor
+  }
+  return(c(fx, fy))
+}
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+p$velocidad<-numeric(n)
+for (iter in 1:tmax) {
+  f <- foreach(i = 1:n, .combine=c) %dopar% fuerza(i)
+  delta <- 0.02 / max(abs(f)) # que nadie desplace una paso muy largo
+  p$x <- foreach(i = 1:n, .combine=c) %dopar% max(min(p[i,]$x + delta * f[c(TRUE, FALSE)][i]/p[i,]$m, 1), 0)
+  p$y <- foreach(i = 1:n, .combine=c) %dopar% max(min(p[i,]$y + delta * f[c(FALSE, TRUE)][i]/p[i,]$m, 1), 0)
+  velocidad<-foreach(i = 1:n, .combine=c) %dopar% (sqrt((delta *f[c(TRUE, FALSE)][i]/p[i,]$m)^2 + (delta *f[c(FALSE, TRUE)][i]/p[i,]$m)^2))
+  p$velocidad <- p$velocidad + velocidad
+}
